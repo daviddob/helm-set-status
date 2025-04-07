c8938109053720b420bd829885b9515b6a69e213
David Dobmeier <david.dobmeier@dcsg.com>
2025-04-07 19:57:44 -0400

    update install script to use tabs

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -12,8 +12,8 @@ export GREP_COLOR="never"
 # need a Unix path
 
 if command -v cygpath >/dev/null 2>&1; then
-  HELM_BIN="$(cygpath -u "${HELM_BIN}")"
-  HELM_PLUGIN_DIR="$(cygpath -u "${HELM_PLUGIN_DIR}")"
+	HELM_BIN="$(cygpath -u "${HELM_BIN}")"
+	HELM_PLUGIN_DIR="$(cygpath -u "${HELM_PLUGIN_DIR}")"
 fi
 
 [ -z "$HELM_BIN" ] && HELM_BIN=$(command -v helm)
@@ -25,122 +25,122 @@ mkdir -p "$HELM_HOME"
 : "${HELM_PLUGIN_DIR:="$HELM_HOME/plugins/$PROJECT_NAME"}"
 
 if [ "$SKIP_BIN_INSTALL" = "1" ]; then
-  echo "Skipping binary install"
-  exit
+	echo "Skipping binary install"
+	exit
 fi
 
 # which mode is the common installer script running in
 SCRIPT_MODE="install"
 if [ "$1" = "-u" ]; then
-  SCRIPT_MODE="update"
+	SCRIPT_MODE="update"
 fi
 
 # initArch discovers the architecture for this system.
 initArch() {
-  ARCH=$(uname -m)
-  case $ARCH in
-  armv5*) ARCH="armv5" ;;
-  armv6*) ARCH="armv6" ;;
-  armv7*) ARCH="armv7" ;;
-  aarch64) ARCH="arm64" ;;
-  x86) ARCH="386" ;;
-  x86_64) ARCH="amd64" ;;
-  i686) ARCH="386" ;;
-  i386) ARCH="386" ;;
-  esac
+	ARCH=$(uname -m)
+	case $ARCH in
+	armv5*) ARCH="armv5" ;;
+	armv6*) ARCH="armv6" ;;
+	armv7*) ARCH="armv7" ;;
+	aarch64) ARCH="arm64" ;;
+	x86) ARCH="386" ;;
+	x86_64) ARCH="amd64" ;;
+	i686) ARCH="386" ;;
+	i386) ARCH="386" ;;
+	esac
 }
 
 # initOS discovers the operating system for this system.
 initOS() {
-  OS=$(uname -s)
+	OS=$(uname -s)
 
-  case "$OS" in
-  Windows_NT) OS='windows' ;;
-  # Msys support
-  MSYS*) OS='windows' ;;
-  # Minimalist GNU for Windows
-  MINGW*) OS='windows' ;;
-  CYGWIN*) OS='windows' ;;
-  Darwin) OS='macos' ;;
-  Linux) OS='linux' ;;
-  esac
+	case "$OS" in
+	Windows_NT) OS='windows' ;;
+	# Msys support
+	MSYS*) OS='windows' ;;
+	# Minimalist GNU for Windows
+	MINGW*) OS='windows' ;;
+	CYGWIN*) OS='windows' ;;
+	Darwin) OS='macos' ;;
+	Linux) OS='linux' ;;
+	esac
 }
 
 # verifySupported checks that the os/arch combination is supported for
 # binary builds.
 verifySupported() {
-  supported="linux-amd64\nlinux-arm64\nfreebsd-amd64\nmacos-amd64\nmacos-arm64\nwindows-amd64"
-  if ! echo "${supported}" | grep -q "${OS}-${ARCH}"; then
-    echo "No prebuild binary for ${OS}-${ARCH}."
-    exit 1
-  fi
+	supported="linux-amd64\nlinux-arm64\nfreebsd-amd64\nmacos-amd64\nmacos-arm64\nwindows-amd64"
+	if ! echo "${supported}" | grep -q "${OS}-${ARCH}"; then
+		echo "No prebuild binary for ${OS}-${ARCH}."
+		exit 1
+	fi
 
-  if
-    ! command -v curl >/dev/null 2>&1 && ! command -v wget >/dev/null 2>&1
-  then
-    echo "Either curl or wget is required"
-    exit 1
-  fi
+	if
+		! command -v curl >/dev/null 2>&1 && ! command -v wget >/dev/null 2>&1
+	then
+		echo "Either curl or wget is required"
+		exit 1
+	fi
 }
 
 # getDownloadURL checks the latest available version.
 getDownloadURL() {
-  version=$(git -C "$HELM_PLUGIN_DIR" describe --tags --exact-match 2>/dev/null || :)
-  if [ "$SCRIPT_MODE" = "install" ] && [ -n "$version" ]; then
-    DOWNLOAD_URL="https://github.com/$PROJECT_GH/releases/download/$version/$PROJECT_NAME-$OS-$ARCH.tgz"
-  else
-    DOWNLOAD_URL="https://github.com/$PROJECT_GH/releases/latest/download/$PROJECT_NAME-$OS-$ARCH.tgz"
-  fi
+	version=$(git -C "$HELM_PLUGIN_DIR" describe --tags --exact-match 2>/dev/null || :)
+	if [ "$SCRIPT_MODE" = "install" ] && [ -n "$version" ]; then
+		DOWNLOAD_URL="https://github.com/$PROJECT_GH/releases/download/$version/$PROJECT_NAME-$OS-$ARCH.tgz"
+	else
+		DOWNLOAD_URL="https://github.com/$PROJECT_GH/releases/latest/download/$PROJECT_NAME-$OS-$ARCH.tgz"
+	fi
 }
 
 # Temporary dir
 mkTempDir() {
-  HELM_TMP="$(mktemp -d -t "${PROJECT_NAME}-XXXXXX")"
+	HELM_TMP="$(mktemp -d -t "${PROJECT_NAME}-XXXXXX")"
 }
 rmTempDir() {
-  if [ -d "${HELM_TMP:-/tmp/helm-set-status-tmp}" ]; then
-    rm -rf "${HELM_TMP:-/tmp/helm-set-status-tmp}"
-  fi
+	if [ -d "${HELM_TMP:-/tmp/helm-set-status-tmp}" ]; then
+		rm -rf "${HELM_TMP:-/tmp/helm-set-status-tmp}"
+	fi
 }
 
 # downloadFile downloads the latest binary package and also the checksum
 # for that binary.
 downloadFile() {
-  PLUGIN_TMP_FILE="${HELM_TMP}/${PROJECT_NAME}.tgz"
-  echo "Downloading $DOWNLOAD_URL"
-  if
-    command -v curl >/dev/null 2>&1
-  then
-    curl -sSf -L "$DOWNLOAD_URL" >"$PLUGIN_TMP_FILE"
-  elif
-    command -v wget >/dev/null 2>&1
-  then
-    wget -q -O - "$DOWNLOAD_URL" >"$PLUGIN_TMP_FILE"
-  fi
+	PLUGIN_TMP_FILE="${HELM_TMP}/${PROJECT_NAME}.tgz"
+	echo "Downloading $DOWNLOAD_URL"
+	if
+		command -v curl >/dev/null 2>&1
+	then
+		curl -sSf -L "$DOWNLOAD_URL" >"$PLUGIN_TMP_FILE"
+	elif
+		command -v wget >/dev/null 2>&1
+	then
+		wget -q -O - "$DOWNLOAD_URL" >"$PLUGIN_TMP_FILE"
+	fi
 }
 
 # installFile verifies the SHA256 for the file, then unpacks and
 # installs it.
 installFile() {
-  tar xzf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
-  HELM_TMP_BIN="$HELM_TMP/$PROJECT_NAME/bin/$PROJECT_NAME"
-  if [ "${OS}" = "windows" ]; then
-    HELM_TMP_BIN="$HELM_TMP_BIN.exe"
-  fi
-  echo "Preparing to install into ${HELM_PLUGIN_DIR}"
-  mkdir -p "$HELM_PLUGIN_DIR/bin"
-  cp "$HELM_TMP_BIN" "$HELM_PLUGIN_DIR/bin"
+	tar xzf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
+	HELM_TMP_BIN="$HELM_TMP/$PROJECT_NAME/bin/$PROJECT_NAME"
+	if [ "${OS}" = "windows" ]; then
+		HELM_TMP_BIN="$HELM_TMP_BIN.exe"
+	fi
+	echo "Preparing to install into ${HELM_PLUGIN_DIR}"
+	mkdir -p "$HELM_PLUGIN_DIR/bin"
+	cp "$HELM_TMP_BIN" "$HELM_PLUGIN_DIR/bin"
 }
 
 # exit_trap is executed if on exit (error or not).
 exit_trap() {
-  result=$?
-  rmTempDir
-  if [ "$result" != "0" ]; then
-    echo "Failed to install $PROJECT_NAME"
-    printf "\tFor support, go to https://github.com/${PROJECT_GH}.\n"
-  fi
-  exit $result
+	result=$?
+	rmTempDir
+	if [ "$result" != "0" ]; then
+		echo "Failed to install $PROJECT_NAME"
+		printf "\tFor support, go to https://github.com/${PROJECT_GH}.\n"
+	fi
+	exit $result
 }
 
 # Execution
